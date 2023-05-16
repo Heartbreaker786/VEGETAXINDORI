a5f17a5d907c5cfd0231c29f6b6d2642257c5796
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-16 21:20:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RohitXBot/RohitXMusic",
+    "https://github.com/Heartbreaker786/VEGETAXINDORI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
